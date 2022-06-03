757d6f1eb077e43f332065d70ea44713aa51685e
Hestia |LagzalMohamed <godhestia0@gmail.com>
2022-06-03 11:43:41 +0100

    COMMIT 2

--- inc/view/content/login.cont.php
+++
@@ -1,7 +0,0 @@
-<form class="login" method="POST" action="?action=login" >
-    <input type="text" name="usr" placeholder="USERNAME">
-    <br/>
-    <input type="text" name="passwd" placeholder="PASSWORD">
-    <br/>
-    <input id="LOGIN_BOTTON" type="submit" name="GO" value="LOGIN">
-</form>
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    session_start();
-    require_once './inc/ctr/ctr.inc.php';
-    perform();
